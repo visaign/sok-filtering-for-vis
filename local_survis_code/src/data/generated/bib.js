280209f820987319758eff93c3c9da4b2c68d0ec
CarlottaGuiso <61839257+CarlottaGuiso@users.noreply.github.com>
2023-10-01 16:08:01 +0200

    upload survis

---
+++ local_survis_code/src/data/generated/bib.js
@@ -0,0 +1,82 @@
+﻿define({ entries : {
+    "10.1145/3379337.3415813": {
+        "author": "Setlur, Vidya and Hoque, Enamul and Kim, Dae Hyun and Chang, Angel X.",
+        "doi": "10.1145/3379337.3415813",
+        "keywords": "feedback:yes, guidance:no, spatial distribution:unified, type:filter interface dominated ,",
+        "title": "Sneak Pique: Exploring Autocompletion as a Data Discovery Scaffold for Supporting Visual Analysis",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "10155238": {
+        "author": "Eirich, Joscha and J\u00e4ckle, Dominik and Sedlmair, Michael and Wehner, Christoph and Schmid, Ute and Bernard, J\u00fcrgen and Schreck, Tobias",
+        "doi": "10.1109/TVCG.2023.3279857",
+        "keywords": "feedback:no, guidance:no, spatial distribution:distributed, type:direct manipulation dominated ,",
+        "title": "ManuKnowVis: How to Support Different User Groups in Contextualizing and Leveraging Knowledge Repositories",
+        "type": "ARTICLE",
+        "year": "2023"
+    },
+    "9695348": {
+        "author": "Zhang, Wei and Tan, Siwei and Chen, Siming and Meng, Linghao and Zhang, Tianye and Zhu, Rongchen and Chen, Wei",
+        "doi": "10.1109/TVCG.2022.3146508",
+        "keywords": "feedback:no, guidance:no, spatial distribution:distributed compact, type:filter interface dominated",
+        "title": "Visual Reasoning for Uncertainty in Spatio-Temporal Events of Historical Figures",
+        "type": "ARTICLE",
+        "year": "2023"
+    },
+    "9729541": {
+        "author": "Deng, Dazhen and Wu, Yihong and Shu, Xinhuan and Wu, Jiang and Fu, Siwei and Cui, Weiwei and Wu, Yingcai",
+        "doi": "10.1109/TVCG.2022.3155440",
+        "keywords": "feedback:no, guidance:no, spatial distribution:distributed, type:filter interface dominated ,",
+        "title": "VisImages: A Fine-Grained Expert-Annotated Visualization Dataset",
+        "type": "ARTICLE",
+        "year": "2023"
+    },
+    "9767783": {
+        "author": "Mancinelli, Claudio and Nazzaro, Giacomo and Pellacini, Fabio and Puppo, Enrico",
+        "doi": "10.1109/TVCG.2022.3171179",
+        "keywords": "feedback: , guidance: , spatial distribution: , type:direct manipulation dominated ,",
+        "title": "b/Surf: Interactive B\u00e9zier Splines on Surface Meshes",
+        "type": "ARTICLE",
+        "year": "2023"
+    },
+    "9904933": {
+        "author": "Yu, Yuncong and Kruyff, Dylan and Jiao, Jiao and Becker, Tim and Behrisch, Michael",
+        "doi": "10.1109/TVCG.2022.3209431",
+        "keywords": "feedback:no, guidance:no, spatial distribution:distributed, type:filter interface dominated ,",
+        "title": "PSEUDo: Interactive Pattern Search in Multivariate Time Series with Locality-Sensitive Hashing and Relevance Feedback",
+        "type": "ARTICLE",
+        "year": "2023"
+    },
+    "ALEITE202011": {
+        "author": "Roger A.\u00a0Leite and Alessio Arleo and Johannes Sorger and Theresia Gschwandtner and Silvia Miksch",
+        "doi": "https://doi.org/10.1016/j.visinf.2020.09.006",
+        "keywords": "feedback:yes, guidance:yes, spatial distribution:distributed, type:filter interface dominated ,",
+        "title": "Hermes: Guidance-enriched Visual Analytics for economic network exploration",
+        "type": "article",
+        "year": "2020"
+    },
+    "PICCOLOTTO202251": {
+        "author": "Nikolaus Piccolotto and Markus B\u00f6gl and Theresia Gschwandtner and Christoph Muehlmann and Klaus Nordhausen and Peter Filzmoser and Silvia Miksch",
+        "doi": "https://doi.org/10.1016/j.visinf.2022.10.002",
+        "keywords": "feedback:yes, guidance:no, spatial distribution:distributed, type:direct manipulation dominated ,",
+        "title": "TBSSvis: Visual analytics for Temporal Blind Source Separation",
+        "type": "article",
+        "year": "2022"
+    },
+    "https://doi.org/10.1111/cgf.13697": {
+        "author": "van Dortmont, M.A.M.M. and van den Elzen, S. and van Wijk, J.J.",
+        "doi": "https://doi.org/10.1111/cgf.13697",
+        "keywords": "feedback:yes, guidance:no, spatial distribution:distributed, type:filter interface dominated",
+        "title": "ChronoCorrelator: Enriching Events with Time Series",
+        "type": "article",
+        "year": "2019 ,"
+    },
+    "https://doi.org/10.1111/j.1467-8659.2012.03116.x": {
+        "author": "Booshehrian, Maryam and M\u00f6ller, Torsten and Peterman, Randall M. and Munzner, Tamara",
+        "doi": "https://doi.org/10.1111/j.1467-8659.2012.03116.x",
+        "keywords": "feedback:yes, guidance:no, spatial distribution:distributed compact, type:filter interface dominated",
+        "title": "Vismon: Facilitating Analysis of Trade-Offs, Uncertainty, and Sensitivity In Fisheries Management Decision Making",
+        "type": "article",
+        "year": "2012 ,"
+    }
+}});
