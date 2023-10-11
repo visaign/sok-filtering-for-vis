3cc7cedc96f4f4fc85bfbc0f17ce289e64adadbb
CarlottaGuiso <61839257+CarlottaGuiso@users.noreply.github.com>
2023-10-11 16:27:53 +0200

    Add files via upload

---
+++ data/authorized_tags.js
@@ -0,0 +1,19 @@
+define({
+	authorizedTags: {
+		"type:system": {
+			"description": "paper describing a system"
+		},
+		"feedback":{
+			"description": "has the system filters with feedback?"
+		},
+		"guidance":{
+			"description": "has the system filters with guidance?"
+		},
+		"spatial distribution":{
+			"description": "type of spatial distribution of filters"
+		},
+		"type":{
+			"description": "type of system"
+		}
+	}
+});
